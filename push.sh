f79be1d3d43c2da2dc0c732f60068a99ab2a36b6
aubreymalabie <aubrey@iqlab.africa>
2024-08-03 14:26:14 +0200

    testing push script

--- push.sh
+++ push.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 message=$1
+echo 🔴 Parameters required: ssh_key_path, repository_ssh_url, commit_message
 
 if [ -z "$1" ]; then
+  echo "👿 Please enter required parameters; (SSH key path, repo SSH url and commit message 👿"
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "👿 Please enter repository SSH url and commit message 👿"
+  exit 1
+fi
+if [ -z "$3" ]; then
   echo "👿 Please enter commit message 👿"
   exit 1
 fi
 echo COMMIT - 🎽🎽 - add and commit the code
 
 git add .
-git commit -m "$1"
+git commit -m "$3"
 
 echo 🎽🎽🎽🎽 push the code ... using SSH Key ...
 
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/i_account2 && ssh -T git@github.com
+ssh-add $1 && ssh -T git@github.com
 
 echo 🍎 🍎 🍎 SET REMOTE SSH URL ...
-git remote set-url origin git@github.com:iqlab-africa/starter-example.git
+git remote set-url origin $2
 
 echo 🍎 🍎 🍎 PUSH, SKOROKORO!!
 git push
 
 echo DONE!! 🥬 🥬 🥬 🥬 🥬 🥬 🥬 🥬 🥬 🥬 🥬 
+
+# git@github.com:iqlab-africa/starter-example.git
+# ~/.ssh/i_account2
